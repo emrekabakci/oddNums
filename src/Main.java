98b391deedb55912ee1897f755dbfe0e88cd39fb
emrekabakci <emrekabakci.2@icloud.com>
2023-12-17 16:12:45 +0300

    Comment added.

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //Variables defined.
         int num;
         int total = 0;
+
+        //Scanner created.
         Scanner inp = new Scanner(System.in);
 
+        //It will work until an odd number is entered.
         do {
-            System.out.print("Bir sayı girin: ");
+            System.out.print("Enter Number: ");
             num = inp.nextInt();
-            if (num % 4 == 0 || num % 2 == 0) {
+            if (num % 2 == 0) {
                 total += num;
             }
         }
